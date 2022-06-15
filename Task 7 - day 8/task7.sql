5603c8aefabed40f14a24c81b61bdee6c40c5ae5
workytip <106924253+workytip@users.noreply.github.com>
2022-06-15 11:57:08 +0200

    DB file
    please import the data base file task7.sql before running the code ..

---
+++ Task 7 - day 8/task7.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3308
+-- Generation Time: Jun 15, 2022 at 09:54 AM
+-- Server version: 8.0.18
+-- PHP Version: 7.4.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `task7`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+CREATE TABLE IF NOT EXISTS `items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `article` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `granularity` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `timestamp` int(11) NOT NULL,
+  `access` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `agent` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `views` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
